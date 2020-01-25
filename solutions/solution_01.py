857763185e653f454d62b13ae0d86e392f89c582
dexter2206 <dexter2206@gmail.com>
2020-01-25 14:28:09 +0100

    Add solution to exercise 1

---
+++ solutions/solution_01.py
@@ -0,0 +1,23 @@
+from bson.json_util import loads
+from pymongo import MongoClient
+
+
+if __name__ == "__main__":
+    # WARNING!!!!
+    # THIS
+    # DELETES
+    # EVERYTHING
+    with MongoClient() as client:
+        products_db = client["products-db"]["products-info"].drop()
+
+    with open("products.json") as products_file:
+        documents = [loads(line) for line in products_file]
+
+    with MongoClient() as client:
+        products_info = client["products-db"]["products-info"]
+
+        result = products_info.insert_many(documents)
+
+        for _id in result.inserted_ids:
+            product = products_info.find_one({"_id": _id})
+            print(product["name"])
